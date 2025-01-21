10cab5af31759ed9e318952ec0fdaf88010c8c2e
snowSakura520 <3246466094@qq.com>
2025-01-21 15:27:12 +0800

    test1

--- src/ColorfulStarryNight/test.java
+++ src/ColorfulStarryNight/test.java
@@ -9,25 +9,24 @@ import mindustry.ui.dialogs.*;
 public class test extends Mod{
 
     public test(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
+//        Log.info("Loaded ExampleJavaMod constructor.");
+//
+//        //listen for game load event
+//        Events.on(ClientLoadEvent.class, e -> {
+//            //show dialog upon startup
+//            Time.runTask(10f, () -> {
+//                BaseDialog dialog = new BaseDialog("frog");
+//                dialog.cont.add("behold").row();
+//                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+//                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+//                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+//                dialog.show();
+//            });
+//        });
     }
 
     @Override
     public void loadContent(){
-        Log.info("null");
         testItem.load();
     }
 
