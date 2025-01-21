51f94c084affdd8110ee1b9a8910ad0a6a441f4d
snowSakura520 <3246466094@qq.com>
2025-01-22 00:40:30 +0800

    test1

--- src/ColorfulStarryNight/testItem.java
+++ src/ColorfulStarryNight/testItem.java
@@ -4,9 +4,9 @@ import arc.graphics.Color;
 import mindustry.type.Item;
 
 public class testItem {
-    public static Item starburn;
+    public static Item 星燃;
     public static void load() {
-        starburn = new Item("星燃", Color.valueOf("ff0000ff")){{
+        星燃 = new Item("星燃", Color.valueOf("ff0000ff")){{
             explosiveness = 0;
             flammability = 5.5f;
             radioactivity = 2.75f;
