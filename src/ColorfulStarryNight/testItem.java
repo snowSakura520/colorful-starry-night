ddbd2938f586de53c7a7ed71511525eb1e5a501a
snowSakura520 <3246466094@qq.com>
2025-01-22 00:26:47 +0800

    test1

--- src/ColorfulStarryNight/testItem.java
+++ src/ColorfulStarryNight/testItem.java
@@ -8,7 +8,7 @@ public class testItem {
     public static void load() {
         星燃 = new Item("星燃", Color.valueOf("ff0000ff")){{
             explosiveness = 0;
-            flammability = 0;
+            flammability = 5.5f;
             radioactivity = 2.75f;
             charge = 0;
             cost = 1;
